f6d5f356e2ce00bd311f6771284711c5e4c5b5cf
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-04-23 19:59:38 +0300

    Changed name of the table to 'users'

--- server/src/migration/1682213379356-NewUserTable.ts
+++ server/src/migration/1682213379356-NewUserTable.ts
@@ -4,11 +4,11 @@ export class NewUserTable1682213379356 implements MigrationInterface {
     name = 'NewUserTable1682213379356'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
+        await queryRunner.query(`DROP TABLE "users"`);
     }
 
 }
--- server/src/migration/1682215797089-MadeSomeFieldsNullable.ts
+++ server/src/migration/1682215797089-MadeSomeFieldsNullable.ts
@@ -4,13 +4,13 @@ export class MadeSomeFieldsNullable1682215797089 implements MigrationInterface {
     name = 'MadeSomeFieldsNullable1682215797089'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "firstName" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "lastName" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "firstName" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "lastName" DROP NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "lastName" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "firstName" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "lastName" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "firstName" SET NOT NULL`);
     }
 
 }
