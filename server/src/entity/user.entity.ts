a084baafc9657ccf63ef22b17307e35c9eb58410
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-04-23 04:32:08 +0300

    Added new entity User and new table User based on this entity

---
+++ server/src/entity/user.entity.ts
@@ -0,0 +1,17 @@
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id?: number;
+
+    @Column({ type: 'varchar' })
+    email!: string;
+
+    @Column({ type: 'varchar' })
+    firstName!: string;
+
+    @Column({ type: 'varchar' })
+    lastName!: string;
+
+}
