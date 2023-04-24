da35336e8d76e3f0ed5096b138560c56554202ad
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-04-24 06:05:15 +0300

    Changed generatePDF function, now images are also generated

--- server/src/utilities/pdf.utility.ts
+++ server/src/utilities/pdf.utility.ts
@@ -2,6 +2,7 @@ import {User} from "../entities/user.entity";
 import {PDFDocument, PDFFont, PDFImage, PDFPage, StandardFonts} from 'pdf-lib';
 import * as fs from 'fs';
 
+
 export async function generatePDF(user: User): Promise<Buffer> {
 
     const pdfDoc: PDFDocument = await PDFDocument.create();
@@ -15,23 +16,37 @@ export async function generatePDF(user: User): Promise<Buffer> {
     const textWidth: number = font.widthOfTextAtSize(text, fontSize);
     const textHeight: number = font.heightAtSize(fontSize);
 
-    console.log(text);
-
     page.drawText(text, {
         x: (page.getWidth() - textWidth) / 2,
-        y: (page.getHeight() - textHeight) / 2,
+        y: page.getHeight() - textHeight - 60,
         size: fontSize,
         font: font,
     });
 
-    // const imageBytes: Buffer = fs.readFileSync(`../../public/images/${user.id}.jpg`);
-    // const image: PDFImage = await pdfDoc.embedJpg(imageBytes);
-    // page.drawImage(image, {
-    //     width: image.width,
-    //     height: image.height,
-    // });
+    const imageName: string | undefined = user.image!;
+
+    if (imageName) {
+
+        const imageBytes: Buffer = fs.readFileSync(`../../public/images/${imageName}`);
+        const extension: string = imageName.split(".").slice(-1)[0];
+        let image: PDFImage;
+
+        if (extension === "jpg" || extension === "png") {
+
+            if (extension === "jpg") {
+                image = await pdfDoc.embedJpg(imageBytes);
+            } else {
+                image = await pdfDoc.embedPng(imageBytes);
+            }
+
+            page.drawImage(image, {
+                width: image.width,
+                height: image.height,
+                y: (page.getHeight() - textHeight) / 2,
+            });
+        }
+    }
 
     const pdfBytes: Uint8Array = await pdfDoc.save();
-
     return Buffer.from(pdfBytes);
 }
